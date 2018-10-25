c3e323bf66e61bdeca25ca854c0b0d3dc5eb9ee5
Akinwale Samuel <wale0331@gmail.com>
2018-10-25 20:32:07 +0100

    Update router

--- es6/routes/router.js
+++ es6/routes/router.js
@@ -3,8 +3,6 @@ import path from 'path';
 
 const router = express.Router();
 
-// app.use(bodyParser.urlencoded({ extended: true }));
-
 router.get('/', (req, res) => {
   res.status(200).sendFile(path.join(__dirname, '/../../UI/index.html'));
 });
